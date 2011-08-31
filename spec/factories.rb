caa6a9eac422813313c72dab3d1c40df52031e2a
Conrad Taylor <conradwt@gmail.com>
2011-08-31 02:27:06 -0700

    More factory_girl code cleanup.

--- spec/factories.rb
+++ spec/factories.rb
@@ -13,62 +13,62 @@ FactoryGirl.define do
   end
 
   factory :site do |f|
-    name { Factory.next(:name) }
-    title { Factory.next(:name) }
-    path { Factory.next(:name) }
-    host { Factory.next(:name) }
-    gender "male"
-    email { Factory.next(:email) }
-    twitter "foobar"
-    facebook "http://www.facebook.com/FooBar"
-    blog "http://blog.foo.bar"
+    name          { FactoryGirl.generate(:name) }
+    title         { FactoryGirl.generate(:name) }
+    path          { FactoryGirl.generate(:name) }
+    host          { FactoryGirl.generate(:name) }
+    gender        "male"
+    email         { FactoryGirl.generate(:email) }
+    twitter       "foobar"
+    facebook      "http://www.facebook.com/FooBar"
+    blog          "http://blog.foo.bar"
   end
 
   factory :user do |f|
-    provider "twitter"
-    uid { Factory.next(:uid) }
-    name "Foo bar"
-    email { Factory.next(:email) }
-    bio "This is Foo bar's biography."
-    association :site
+    provider      "twitter"
+    uid           { FactoryGirl.generate(:uid) }
+    name          "Foo bar"
+    email         { FactoryGirl.generate(:email) }
+    bio           "This is Foo bar's biography."
+    site
   end
 
   factory :category do
-    name { Factory.next(:name) }
+    name          { FactoryGirl.generate(:name) }
   end
 
   factory :project do
-    name "Foo bar"
-    association  :site
-    assocication :user
-    association  :category
-    about        "Foo bar"
-    headline     "Foo bar"
-    goal         10000
-    expires_at { 1.month.from_now }
-    video_url 'http://vimeo.com/17298435'
+    name          "Foo bar"
+    site
+    user
+    category
+    about         "Foo bar"
+    headline      "Foo bar"
+    goal          10000
+    expires_at    { 1.month.from_now }
+    video_url     'http://vimeo.com/17298435'
   end
 
   factory :reward do
-    association   :project
+    project
     minimum_value 1.00
     description   "Foo bar"
   end
 
   factory :backer do
-    association :project
-    association :user
-    association :site
-    confirmed true
-    value 10.00
+    project
+    user
+    site
+    confirmed     true
+    value         10.00
   end
 
   factory :oauth_provider do
-    name 'GitHub'
-    strategy 'GitHub'
-    path 'github'
-    key 'test_key'
-    secret 'test_secret'
+    name          'GitHub'
+    strategy      'GitHub'
+    path          'github'
+    key           'test_key'
+    secret        'test_secret'
   end
 
   factory :configuration do
