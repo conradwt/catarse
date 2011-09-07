6bcb9284d306557a9a5819ae2576da03047c4671
Conrad Taylor <conradwt@gmail.com>
2011-09-07 01:34:04 -0700

    Add some additional auth providers.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,8 +2,24 @@ require 'omniauth/openid'
 require 'openid/store/filesystem'
 
 Rails.application.config.middleware.use OmniAuth::Builder do  
-  # provider :open_id, OpenID::Store::Filesystem.new("#{Rails.root}/tmp"), :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'
-  provider :open_id, nil, :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'
+  
+  # providers with id/secret, you need to sign up for their services (see below) and enter the parameters here
+  # provider :facebook, 'APP_ID', 'APP_SECRET'
+  # provider :twitter, 'CONSUMER_KEY', 'CONSUMER_SECRET'
+  # provider :github, 'CLIENT ID', 'SECRET'
+  
+  # generic openid
+  provider :openid, OpenID::Store::Filesystem.new('./tmp'), :name => 'openid'
+  
+  # dedicated openid
+  provider :openid, OpenID::Store::Filesystem.new('./tmp'), :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'
+  # provider :google_apps, OpenID::Store::Filesystem.new('./tmp'), :name => 'google_apps'
+  # /auth/google_apps; you can bypass the prompt for the domain with /auth/google_apps?domain=somedomain.com
+  
+  provider :openid, OpenID::Store::Filesystem.new('./tmp'), :name => 'yahoo', :identifier => 'yahoo.com' 
+  # provider :openid, OpenID::Store::Filesystem.new('./tmp'), :name => 'aol', :identifier => 'openid.aol.com'
+  # provider :openid, OpenID::Store::Filesystem.new('./tmp'), :name => 'myopenid', :identifier => 'myopenid.com'
+  
   begin
     OauthProvider.all.each do |p|
       # This hack can be removed after the upgrade to omniauth 2.0
