7f3eee0f92fd3bfa6787a3cdeb3c9a4c33c6ae5d
Conrad Taylor <conradwt@gmail.com>
2011-09-14 02:10:25 -0700

    Let's try it again.

--- db/migrate/20110914082940_add_categories.rb
+++ db/migrate/20110914082940_add_categories.rb
@@ -1,7 +1,19 @@
 class AddCategories < ActiveRecord::Migration
+  
   def self.up
+    
+    # Add some educational categories.
+    [ 'High School', 'Undergraduate', 'Graduate' ].each do | category |
+      Category.create!( :name => category )
+    end
+      
   end
 
   def self.down
+    
+    # Remove all the records.
+    Category.destroy_all
+    
   end
+  
 end
