5fb304171c5e3cddaed62618570c0743579c5488
Conrad Taylor <conradwt@gmail.com>
2011-09-16 08:15:17 -0700

    Update the timed keyup.

--- app/assets/javascripts/timedKeyup.js
+++ app/assets/javascripts/timedKeyup.js
@@ -1,8 +1,12 @@
-$.fn.timedKeyup = function(handler, timeout){
-  if(timeout == null)
-    timeout = 300
-  $(this).keyup(function(data){
-      $(this).stopTime("keyup")
-      $(this).oneTime(timeout, "keyup", handler, 100);
-    })
-}
+(function($) {
+
+  $.fn.timedKeyup = function(handler, timeout){
+    if(timeout == null)
+      timeout = 300
+    $(this).keyup(function(data){
+        $(this).stopTime("keyup")
+        $(this).oneTime(timeout, "keyup", handler, 100);
+      })
+  }
+  
+})(jQuery);
