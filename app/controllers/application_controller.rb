f8a3bc5e4fab9f3caa89585e42f69f4841f2d30a
Conrad Taylor <conradwt@gmail.com>
2011-09-19 20:50:43 -0700

    Revert locale code and think more about the design.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,12 +25,12 @@ class ApplicationController < ActionController::Base
   # TODO remove this when we launch the new Multidão
   
   def set_locale
-    # return unless params[:locale]
-    # I18n.locale = params[:locale]
-    # return unless current_user
-    # current_user.update_attribute :locale, params[:locale] if params[:locale] != current_user.locale  
-    
-    I18n.locale = (current_user.locale if current_user) || session[:locale] || I18n.default_locale
+    return unless params[:locale]
+    I18n.locale = params[:locale]
+    return unless current_user
+    current_user.update_attribute :locale, params[:locale] if params[:locale] != current_user.locale
+
+    # I18n.locale = (current_user.locale if current_user) || session[:locale] || I18n.default_locale
     
   end
   
