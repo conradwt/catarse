51d7f516276b1507198c84a5a45f8aee35c7ad23
Conrad Taylor <conradwt@gmail.com>
2011-09-19 20:00:18 -0700

    Add some pre-configuration for the bullet gem.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,4 +30,19 @@ Catarse::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+  
+  # Enable the bullet gem for development.
+  # config.after_initialize do
+  #   Bullet.enable = true
+  #   Bullet.alert = true
+  #   Bullet.bullet_logger = true
+  #   Bullet.console = true
+  #   Bullet.growl = true # https://gist.github.com/300184
+  #   Bullet.xmpp = { :account => 'bullets_account@jabber.org',
+  #                      :password => 'bullets_password_for_jabber',
+  #                      :receiver => 'your_account@jabber.org',
+  #                      :show_online_status => true }
+  #   Bullet.rails_logger = true
+  #   Bullet.disable_browser_cache = true
+  # end
 end
