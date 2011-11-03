feaec376c03abd74fec7b579e58340d26290fa34
Conrad Taylor <conradwt@gmail.com>
2011-11-03 05:20:45 -0700

    Revert the API key.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -328,7 +328,7 @@ class ProjectsController < ApplicationController
   
   def bitly
     require 'net/http'
-    res = Net::HTTP.start("api.bit.ly", 80) { |http| http.get("/v3/shorten?login=diogob&apiKey=R_61725148ac416a0faeaca4faba1a9090&longUrl=#{CGI.escape(project_url(@project))}") }
+    res = Net::HTTP.start("api.bit.ly", 80) { |http| http.get("/v3/shorten?login=diogob&apiKey=R_76ee3ab860d76d0d1c1c8e9cc5485ca1&longUrl=#{CGI.escape(project_url(@project))}") }
     data = JSON.parse(res.body)['data']
     data['url'] if data
   end
