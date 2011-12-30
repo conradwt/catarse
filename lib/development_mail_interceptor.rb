154b936c19b472a09b5ac1e940df73a0e75c7663
Conrad Taylor <conradwt@gmail.com>
2011-12-30 01:13:29 -0800

    Code cleanup.

--- lib/development_mail_interceptor.rb
+++ lib/development_mail_interceptor.rb
@@ -1,6 +1,8 @@
 class DevelopmentMailInterceptor
-  def self.delivering_email(message)
+  
+  def self.delivering_email( message )
     message.subject = "#{message.to} #{message.subject}"
     message.to = ENV['DEFAULT_EMAIL']
   end
+  
 end
