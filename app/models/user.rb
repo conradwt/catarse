37dc698c86b4490b2a2fb52eab37c7cab5c48f1b
Conrad Taylor <conradwt@gmail.com>
2012-01-22 04:18:18 -0800

    Test setting of the newsletter.

--- app/models/user.rb
+++ app/models/user.rb
@@ -182,9 +182,9 @@ class User < ActiveRecord::Base
   def update_newsletter_subscription
     
     if self.newsletter?
-      Rails.logger.info( "newsletter = #{self.letter.class.to_s}" )
+      Rails.logger.info( "true:  newsletter = #{self.newsletter.class.to_s}" )
     else
-      Rails.logger.info( "newsletter = #{self.letter.class.to_s}" )
+      Rails.logger.info( "false: newsletter = #{self.newsletter.class.to_s}" )
     end
     
     if self.email?
