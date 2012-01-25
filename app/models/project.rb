59d840a4cefdfc4f5f79d20d1fe2a51b43c68b39
Conrad Taylor <conradwt@gmail.com>
2012-01-25 01:07:06 -0800

    Let's generate short url before create/update.

--- app/models/project.rb
+++ app/models/project.rb
@@ -81,7 +81,7 @@ class Project < ActiveRecord::Base
   after_update  :send_update_project_submission_email,    :if => Proc.new { |p| p.user.email? }
   after_create  :send_new_project_confirmation_email,     :if => Proc.new { |p| p.user.email? }
   after_update  :send_update_project_confirmation_email,  :if => Proc.new { |p| p.user.email? }
-  after_create  :generate_short_url
+  before_create  :generate_short_url
   before_update :generate_short_url
   
   def set_project_expiration_date
