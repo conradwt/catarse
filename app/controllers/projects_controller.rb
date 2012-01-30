ab3bb6a5323ba6be5d3c1f8bcfd25c939fd6843b
Conrad Taylor <conradwt@gmail.com>
2012-01-30 06:57:22 -0800

    Rework the logic to create a project's short url.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -68,7 +68,8 @@ class ProjectsController < ApplicationController
     
     unless @project.new_record?
       @project.reload
-      # @project.update_attribute( :short_url, URLShortener.shorten( project_url( @project ) ) )
+      @project.short_url = URLShortener.shorten( project_url( @project ) )
+      @project.save
       @project.projects_sites.create :site => current_site
     end
 
--- app/models/project.rb
+++ app/models/project.rb
@@ -87,7 +87,7 @@ class Project < ActiveRecord::Base
   before_update  :send_update_project_confirmation_email,  :if => Proc.new { |p| p.user.email? }
   
   after_create   :generate_short_url
-  after_update   :generate_short_url
+  before_update  :generate_short_url
   
   def set_project_expiration_date
     self.expires_at += 23.hours + 59.minutes + 59.seconds
