3c57fc7205893cdd3a9ef0820d5bdfb69d90ef09
Conrad Taylor <conradwt@gmail.com>
2012-05-11 15:18:24 -0700

    Add some debugging primitives.

--- app/models/project.rb
+++ app/models/project.rb
@@ -84,8 +84,8 @@ class Project < ActiveRecord::Base
   before_save   :set_project_expiration_date
   before_update :set_project_expiration_date
   
-  before_create  :send_new_project_submission_email,       :if => Proc.new { |p| p.user.email? && p.reviewable? }
-  before_update  :send_update_project_submission_email,    :if => Proc.new { |p| p.user.email? && p.reviewable? }
+  before_create  :send_new_project_submission_email,       :if => Proc.new { |p| p.user.email? }
+  before_update  :send_update_project_submission_email,    :if => Proc.new { |p| p.user.email? }
   before_create  :send_new_project_confirmation_email,     :if => Proc.new { |p| p.user.email? }
   before_update  :send_update_project_confirmation_email,  :if => Proc.new { |p| p.user.email? }
   
@@ -281,10 +281,12 @@ class Project < ActiveRecord::Base
   private
   
   def send_new_project_submission_email
+    Rails.logger.info( "Entering send_new_submission_email" )
     ProjectsMailer.new_project_submission( self.user, self )
   end
   
   def send_update_project_submission_email
+    Rails.logger.info( "Entering send_new_submission_email" )
     ProjectsMailer.update_project_submission( self.user, self ) unless new_record?
   end
   
