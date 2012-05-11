17a45daede3389e6b792bc2443cffd9dac32c484
Conrad Taylor <conradwt@gmail.com>
2012-05-11 15:30:47 -0700

    Add the reviewable back to the submission e-mails.

--- app/models/project.rb
+++ app/models/project.rb
@@ -84,8 +84,8 @@ class Project < ActiveRecord::Base
   before_save   :set_project_expiration_date
   before_update :set_project_expiration_date
   
-  before_create  :send_new_project_submission_email,       :if => Proc.new { |p| p.user.email? }
-  before_update  :send_update_project_submission_email,    :if => Proc.new { |p| p.user.email? }
+  before_create  :send_new_project_submission_email,       :if => Proc.new { |p| p.user.email? && p.reviewable? }
+  before_update  :send_update_project_submission_email,    :if => Proc.new { |p| p.user.email? && p.reviewable? }
   before_create  :send_new_project_confirmation_email,     :if => Proc.new { |p| p.user.email? }
   before_update  :send_update_project_confirmation_email,  :if => Proc.new { |p| p.user.email? }
   
